6d698058da5fcea34a1ff972b92f77af6d7b92f5
cehali <cezary.haliniarz@gmail.com>
2022-08-05 12:55:22 +0200

    chore: add polygon

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -28,4 +28,16 @@ export const chains = {
     blockExplorerUrls: ["https://evm.evmos.org"],
     exampleContractAddress: "0xDdc3819d5eb83aF1267115a518A354BAdb0B90dF",
   },
+  [137]: {
+    chainId: ethers.utils.hexValue(137),
+    rpcUrls: ["https://polygon-rpc.com"],
+    chainName: "Polygon",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    blockExplorerUrls: ["https://polygonscan.com/"],
+    exampleContractAddress: "0xD661c4D3bb516f62cD13DB90B205cff19e78F09d",
+  },
 };
