1ef1c1cfd0a04e2a0979746d6d8be5eb3ac9fdb8
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-06-04 11:46:19 +0200

    added mainnet for starknet

--- src/pages/StarknetBlock.tsx
+++ src/pages/StarknetBlock.tsx
@@ -81,18 +81,29 @@ export const StarknetBlock = ({ props, network }: Props) => {
           <div className="px-6 py-3 text-sm w-3/5 text-center text-gray-500">
             <i>
               To <b>write</b> a snapshot of current <b>prices</b> to the
-              contract's storage you should have <b>sepoliaETH</b> be added to
-              the wallet as described here:&nbsp;
-              <a
-                target="blank"
-                href="https://book.starknet.io/ch02-05-01-start-with-sepolia.html"
-                referrerPolicy="no-referrer"
-              >
-                <u>
-                  https://book.starknet.io/ch02-05-01-start-with-sepolia.html
-                </u>
-              </a>
+              contract's storage you should have{" "}
+              <b>{network?.nativeCurrency.symbol}</b> be added to the wallet.
+              <br />
+              {network?.label === "Sepolia" && (
+                <span>
+                  For sepolia is described here:&nbsp;
+                  <a
+                    target="blank"
+                    href="https://book.starknet.io/ch02-05-01-start-with-sepolia.html"
+                    referrerPolicy="no-referrer"
+                  >
+                    <u>
+                      https://book.starknet.io/ch02-05-01-start-with-sepolia.html
+                    </u>
+                  </a>
+                </span>
+              )}
             </i>
+            <br />
+            <br />
+            Pay attention to the <b>network</b> you have <b>selected</b> in your{" "}
+            <b>ArgentX </b>
+            extension!
           </div>,
           network && txHash && <ChainTx txHash={txHash} network={network} />,
         ]
